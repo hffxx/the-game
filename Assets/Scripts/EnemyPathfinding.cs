d800a21b3c8f11c5dbedc9227054e01f6a6dc321
hffxx <hoffix0123@gmail.com>
2026-02-19 01:11:31 +0100

    add snake and fix player animation

--- Assets/Scripts/EnemyPathfinding.cs
+++ Assets/Scripts/EnemyPathfinding.cs
@@ -7,15 +7,26 @@ public class EnemyPathfinding : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 moveDirection;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRenderer;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveDirection * (moveSpeed * Time.fixedDeltaTime));
+
+        if (moveDirection != Vector2.zero)
+        {
+            myAnimator.SetFloat("moveX", moveDirection.x);
+            myAnimator.SetFloat("moveY", moveDirection.y);
+            mySpriteRenderer.flipX = moveDirection.x < 0;
+        }
     }
 
     public void MoveTo(Vector2 targetPosition)
